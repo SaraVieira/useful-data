08d61c8c093d9d1a91e14c18f8b5ab8e1565be40
Sara Vieira <hey@iamsaravieira.com>
2022-02-19 18:29:39 +0100

    add gh link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,17 @@
+import { GH } from "./Icons";
 import { ToggleColorButton } from "./ToggleColorButton";
 
 export const Header = () => (
   <header className=" pt-4 pb-8">
-    <div className="flex justify-end pr-4">
+    <div className="flex justify-end gap-3 pr-4 items-center">
+      <a
+        href="https://github.com/SaraVieira/useful-data"
+        target="_blank"
+        rel="noreferrer"
+        className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+      >
+        <GH />
+      </a>
       <ToggleColorButton />
     </div>
     <img src="/logo.png" className="w-12 m-auto block mb-2" alt="JSON Data" />
